34ddf5422b469b8fc1015d13f55e2114516c33cc
Ricka <carvalho@alunos.utfpr.edu.br>
2023-05-18 00:48:21 -0300

    Revert "Movimento e Pulo"
    This reverts commit cc1cf98366f2a7bbdaefebf2c84305fd45cfe81b.

--- Retro Rumble/Assets/Scripts/Player/PlayerControls.cs
+++ Retro Rumble/Assets/Scripts/Player/PlayerControls.cs
@@ -14,85 +14,16 @@ public class PlayerControls : MonoBehaviour
 
     [Header("Grounding")]
 
-    //Movimento
     private float horizontal;
-    private float vertical;
-    bool facingRight;
 
-    //Pulo
-    Rigidbody2D rigidbody;
-    float lastY;
-    bool isJumping;
-
-    void Awake()
-    {
-        rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.Sleep();
-    }
-
-    //Executado de acordo com o editor
     private void FixedUpdate()
     {
-        if(!isJumping)
-        {
-            rb.velocity = new Vector2(horizontal * speed, vertical * speed);
-        }
-        else
-        {
-            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-            if (transform.position.y <= lastY-0.00000000000001)
-            {
-                OnLanding();
-            }
-        }
-        Flip(horizontal);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-    //Executado todo frame
-    void Update()
-    {
-        
-    }
-
-    //Pulo
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && !isJumping)
-        {
-            lastY = transform.position.y;
-            rigidbody.gravityScale = 1.5f;
-            rigidbody.WakeUp();
-            rigidbody.AddForce(new Vector2(transform.position.x + 7.5f, jumpingPower));
-            isJumping = true;
-        }
-    }
-
-    //Aterrisar
-    void OnLanding()
-    {
-        isJumping = false;
-        rigidbody.gravityScale = 0f;
-        rigidbody.Sleep();
-        lastY = transform.position.y;
-    }
-    //Movimento
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
-        vertical = context.ReadValue<Vector2>().y;
 
-    }
-
-    //inverter lado que está olhando
-    private void Flip(float horizontal)
-    {
-        if (horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
-        {
-            facingRight = !facingRight;
-
-            Vector3 scale = transform.localScale;
-            scale.x *= -1;
-            transform.localScale = scale;
-        } 
     }
 }
