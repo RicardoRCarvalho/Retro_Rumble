2de33a9a3c39131bb96051786365b24b960f0407
fatuch19 <fatuch19@gmail.com>
2023-05-21 02:01:07 -0300

    Animações tão pica e começando a configurar os ataques

--- Retro Rumble/Assets/Scripts/Player/PlayerControls.cs
+++ Retro Rumble/Assets/Scripts/Player/PlayerControls.cs
@@ -22,6 +22,11 @@ public class PlayerControls : MonoBehaviour
     private Animator anim;
     private bool isWalking;
     private bool isAttacking;
+
+    //Variaveis de Ataque
+    public GameObject hitboxPoint;
+    public float radiusHitbox;
+    public LayerMask enemies;
     //pulo
     bool isJumping = false;
     float lastY;
@@ -139,10 +144,18 @@ public class PlayerControls : MonoBehaviour
     {
 
         anim.SetBool("isAttacking", true);
-        
+        Collider2D[]enemy = Physics2D.OverlapCircleAll(hitboxPoint.transform.position,radiusHitbox, enemies);
+        foreach(Collider2D enemyGameOBject in enemy)
+        {
+            Debug.Log("HitEnemy");
+        }
 
     }
- 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(hitboxPoint.transform.position, radiusHitbox);
+
+    }
     #endregion
 
 }
