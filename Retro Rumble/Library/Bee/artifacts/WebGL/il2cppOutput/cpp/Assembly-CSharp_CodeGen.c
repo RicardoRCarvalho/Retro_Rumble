cdb483f9a0039d3b190c3562e92c6274e987f425
Ricka <carvalho@alunos.utfpr.edu.br>
2023-05-23 21:01:53 -0300

    Library

---
+++ Retro Rumble/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,97 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void inimigoBasic::Start()
+extern void inimigoBasic_Start_m3EA1C8D5940C1EAB8C18725949352DD8A5752E98 (void);
+// 0x00000002 System.Void inimigoBasic::Update()
+extern void inimigoBasic_Update_mBF05F6091D46649A28682A523D3C7C16EFCDBED6 (void);
+// 0x00000003 System.Void inimigoBasic::.ctor()
+extern void inimigoBasic__ctor_m448444D4C75E44C62302D445384BACB86CEB71FE (void);
+// 0x00000004 System.Void PlayerControls::Start()
+extern void PlayerControls_Start_m31F1265A18F322E8BCD0379BA4ECB57479776660 (void);
+// 0x00000005 System.Void PlayerControls::FixedUpdate()
+extern void PlayerControls_FixedUpdate_m3906A56D69C46B90E11E1F45EE33A43A142896D6 (void);
+// 0x00000006 System.Void PlayerControls::Update()
+extern void PlayerControls_Update_m1FB8C4FBC7768E727B22A107B203C853DBECCC44 (void);
+// 0x00000007 System.Void PlayerControls::Move(UnityEngine.InputSystem.InputAction/CallbackContext)
+extern void PlayerControls_Move_m54E3D06EC1A5E99859090F89AD05BCA1D2CDF872 (void);
+// 0x00000008 System.Void PlayerControls::Jump(UnityEngine.InputSystem.InputAction/CallbackContext)
+extern void PlayerControls_Jump_mD0E0A8709BD5DA88F283783B5CF1AFB38BF0BF49 (void);
+// 0x00000009 System.Void PlayerControls::Flip(System.Single)
+extern void PlayerControls_Flip_mCED3F4FF2E60277283186BA32AFFFA4E6C93E00D (void);
+// 0x0000000A System.Void PlayerControls::onLanding()
+extern void PlayerControls_onLanding_m850963225D35CAD333AE7AC465DC1F20DB4190F0 (void);
+// 0x0000000B System.Void PlayerControls::Attack(UnityEngine.InputSystem.InputAction/CallbackContext)
+extern void PlayerControls_Attack_m118826D8381514442F3936AF7C7899FDEA28353F (void);
+// 0x0000000C System.Void PlayerControls::basicAttack()
+extern void PlayerControls_basicAttack_mD0735A7A1D301BB76B41C77FA4A4F0CC1E12DB25 (void);
+// 0x0000000D System.Void PlayerControls::OnDrawGizmos()
+extern void PlayerControls_OnDrawGizmos_m413D71F426697CEAE935A585803500A9CB3DFD0C (void);
+// 0x0000000E System.Void PlayerControls::.ctor()
+extern void PlayerControls__ctor_m332F79A4E651845074B3ABFAEF8F6F3604244309 (void);
+static Il2CppMethodPointer s_methodPointers[14] = 
+{
+	inimigoBasic_Start_m3EA1C8D5940C1EAB8C18725949352DD8A5752E98,
+	inimigoBasic_Update_mBF05F6091D46649A28682A523D3C7C16EFCDBED6,
+	inimigoBasic__ctor_m448444D4C75E44C62302D445384BACB86CEB71FE,
+	PlayerControls_Start_m31F1265A18F322E8BCD0379BA4ECB57479776660,
+	PlayerControls_FixedUpdate_m3906A56D69C46B90E11E1F45EE33A43A142896D6,
+	PlayerControls_Update_m1FB8C4FBC7768E727B22A107B203C853DBECCC44,
+	PlayerControls_Move_m54E3D06EC1A5E99859090F89AD05BCA1D2CDF872,
+	PlayerControls_Jump_mD0E0A8709BD5DA88F283783B5CF1AFB38BF0BF49,
+	PlayerControls_Flip_mCED3F4FF2E60277283186BA32AFFFA4E6C93E00D,
+	PlayerControls_onLanding_m850963225D35CAD333AE7AC465DC1F20DB4190F0,
+	PlayerControls_Attack_m118826D8381514442F3936AF7C7899FDEA28353F,
+	PlayerControls_basicAttack_mD0735A7A1D301BB76B41C77FA4A4F0CC1E12DB25,
+	PlayerControls_OnDrawGizmos_m413D71F426697CEAE935A585803500A9CB3DFD0C,
+	PlayerControls__ctor_m332F79A4E651845074B3ABFAEF8F6F3604244309,
+};
+static const int32_t s_InvokerIndices[14] = 
+{
+	4194,
+	4194,
+	4194,
+	4194,
+	4194,
+	4194,
+	3451,
+	3451,
+	3387,
+	4194,
+	3451,
+	4194,
+	4194,
+	4194,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	14,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
