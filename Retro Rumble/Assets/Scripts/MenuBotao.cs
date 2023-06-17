87f4a3ba77b42b9693adf53742f5dd04af63b483
fatuch19 <fatuch19@gmail.com>
2023-06-17 14:43:54 -0300

    Artes do menu implementadas

--- Retro Rumble/Assets/Scripts/MenuBotao.cs
+++ Retro Rumble/Assets/Scripts/MenuBotao.cs
@@ -2,19 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MenuBotao : MonoBehaviour
 {
-    public GameObject player2;
-    public GameObject player2UI;
-    public PlayerControls script;
-
+    //  public GameObject player2;
+    //public GameObject player2UI;
+    // public PlayerControls script;
+    public Animator anim;
     public static int numPlayers = 1;
+    private int selectedButton = 1;
 
-    // Start is called before the first frame update
+    public void UpMenu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            anim.SetBool("1Player", true);
+            Debug.Log("click");
+            selectedButton = 1;
+        }
+    }
+    public void DownMenu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            anim.SetBool("1Player", false);
+            Debug.Log("click");
+            selectedButton = 2;
+        }
+    }
+    public void Click(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            anim.SetTrigger("Click");
+        }
+    }
+
     public void SinglePlayer()
     {
-        Debug.Log("click");
+        
         numPlayers = 1;
         SceneManager.LoadScene("Cena1");
 
@@ -22,7 +49,7 @@ public class MenuBotao : MonoBehaviour
     }
     public void MultiPlayer()
     {
-        Debug.Log("click");
+        
         numPlayers = 2;
 
         SceneManager.LoadScene(1);
